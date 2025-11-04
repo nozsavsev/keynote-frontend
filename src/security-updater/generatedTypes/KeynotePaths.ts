f6e9d5f49a27f5777b47d2d4ddeec170a9c8419a
nozsavsev <nozsavsev@gmail.com>
2025-11-04 02:25:17 +0200

    updated page permissions

--- src/security-updater/generatedTypes/KeynotePaths.ts
+++ src/security-updater/generatedTypes/KeynotePaths.ts
@@ -1,14 +1,32 @@
 export const SecurityDescriptor: SecurityDescriptorType = {
-  "/": [],
-  "/403": [],
-  "/404": [],
-  "/500": [],
-  "/account": ["AnyAuthenticated"],
-  "/account/keynotes": ["AnyAuthenticated"],
-  "/_app": [],
-};
-export type KeynotePaths = "/" | "/403" | "/404" | "/500" | "/account" | "/account/keynotes" | "/_app";
+  '/': [],
+  '/403': [],
+  '/404': [],
+  '/500': [],
+  '/account': ['AnyAuthenticated' ],
+  '/account/keynotes': ['AnyAuthenticated' ],
+  '/present': ['AnyAuthenticated'],
+  '/screen': [],
+  '/spectate': [],
+  '/_app': [],
+}
+export type KeynotePaths = 
+  | '/'
+  | '/403'
+  | '/404'
+  | '/500'
+  | '/account'
+  | '/account/keynotes'
+  | '/present'
+  | '/screen'
+  | '/spectate'
+  | '/_app';                                                                         
+                                                
+export type PageAccessRuleType = 
+  | 'AnyAdmin'
+  | 'AnyAuthenticated'
+  | 'RequireVerifiedEmail'
+  | 'PrAdminManageKeynotes'
+  | 'PrUploadFiles'
 
-export type PageAccessRuleType = "AnyAdmin" | "AnyAuthenticated" | "RequireVerifiedEmail" | "PrAdminManageKeynotes" | "PrUploadFiles";
-
-export type SecurityDescriptorType = { [K in KeynotePaths]: PageAccessRuleType[] };
+export type SecurityDescriptorType = { [K in KeynotePaths]: PageAccessRuleType[]; };
